841ee66f6f2c2b55ce9154535fcabbb0e8c6a732
Asher <13ashi@gmail.com>
2017-08-03 20:49:55 -0400

    hello world controller

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using System.Web.Util;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return $"Hello {name}, NumTimes is: {numTimes}";
+        }
+    }
+}
